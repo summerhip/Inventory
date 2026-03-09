8510d253ca760b17a10f940efdd604296ee6c7b5
summerhip <56841221+summerhip@users.noreply.github.com>
2026-03-09 17:03:26 -0400

    fix: trying this

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 3000;
 // Middleware
 const corsOptions = {
   origin: [
-    "https://summerhip.github.io",
+    "https://summerhip.github.io/Inventory",
     "http://localhost:5173",
     "http://localhost:3000",
   ],
