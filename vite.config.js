9d642a2cca2015169b569e3126ee8c6b2e4e858d
summerhip <56841221+summerhip@users.noreply.github.com>
2026-03-09 15:05:13 -0400

    feat: deploy

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/Inventory/",
   plugins: [react()],
   optimizeDeps: {
     include: ["ag-grid-community", "ag-grid-react"],
